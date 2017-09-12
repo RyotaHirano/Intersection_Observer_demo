5b585a86e37a9bd33b1d1c07f502b90884526137
ryotahirano <hirano@tsumikiinc.com>
2017-09-12 14:13:39 +0900

    mod options

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import '../css/style.scss'
 import createIntersectionObserver from './modules/IntersectionObserver'
 
-const options = {
+const IntersectionObserverOptions = {
   targets: '.js-intersection-target',
   addClass: 'is-show'
 }
 
-Array.prototype.forEach.call(document.querySelectorAll(options.targets), el => {
-  const observerEl = new createIntersectionObserver(el, options)
+Array.prototype.forEach.call(document.querySelectorAll(IntersectionObserverOptions.targets), el => {
+  const observerEl = new createIntersectionObserver(el, IntersectionObserverOptions)
   observerEl.bindObserver()
 })
